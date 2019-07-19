aadc05e27e8bc65057ba28dfc27956393f3f13bc
Onder Gunacan <ondergunacan@Onders-MacBook-Air.local>
2019-07-19 13:58:46 -0600

    add production connection

--- knexfile.js
+++ knexfile.js
@@ -6,4 +6,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/sticker-store'
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 };
